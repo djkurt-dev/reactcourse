46909b22c3d7e6383f5b06fa3bc93901078580a1
kurt dev <73378023+djkurtttimajo@users.noreply.github.com>
2021-06-04 20:28:27 +0800

    assignment 1

--- my-app/src/components/DishDetailComponent.js
+++ my-app/src/components/DishDetailComponent.js
@@ -10,16 +10,14 @@ class DishDetail extends Component {
     renderDish(dish) {
         if (dish != null) {
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={dish.image} alt={dish.name}></CardImg>
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
+                <div>
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name}></CardImg>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
                 </div>
             );
         }
