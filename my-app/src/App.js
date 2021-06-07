dbe1981a81a6ef297ecd42ca163a640d4762d134
kurt dev <73378023+djkurtttimajo@users.noreply.github.com>
2021-06-07 11:25:06 +0800

    Presentational and Container Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,13 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import DishDetail from './components/DishDetailComponent';
-import './App.css';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null
-    };
-  }
-
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
-
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Main />
       </div>
     )
   }
