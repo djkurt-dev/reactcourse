ebe4e76c81dd72f6cb30901717bdeaffb823b8e9
kurt dev <73378023+djkurtttimajo@users.noreply.github.com>
2021-06-07 23:29:48 +0800

    switched to func components

--- my-app/src/components/DishDetailComponent.js
+++ my-app/src/components/DishDetailComponent.js
@@ -26,7 +26,7 @@ const RenderDish = ({ dish }) => {
     }
 }
 
-const RenderComments = ({ dish }) => {
+const RenderComments = ({ comments }) => {
 
     if (dish != null) {
         const comments = dish.comments.map((com) => {
