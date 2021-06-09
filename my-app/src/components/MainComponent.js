9ec316c306e255247b73c3dd1b65cbe048b607f5
kurt dev <73378023+djkurtttimajo@users.noreply.github.com>
2021-06-09 17:18:44 +0800

    Assignment 2

--- my-app/src/components/MainComponent.js
+++ my-app/src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -52,6 +53,7 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={() => <Contact />} />
